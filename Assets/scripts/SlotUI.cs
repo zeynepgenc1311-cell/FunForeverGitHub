0fd3fdf5b7eb73ffe43aa3e5f0dcf15d70658ac6
yukiouuu <zeynep.genc1311@gmail.com>
2025-12-08 02:41:28 +0300

    script sildim

--- Assets/scripts/SlotUI.cs
+++ Assets/scripts/SlotUI.cs
@@ -1,42 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-
-public class SlotUI : MonoBehaviour
-{
-    public Image itemImage;
-    public Text itemCountText;
-    public Text priceText;
-
-
-
-
-    [HideInInspector] public Item item;  // Slotta hangi item var
-    public Button button;                // Slot butonu
-
-    private void Start()
-    {
-        button = GetComponent<Button>();
-        button.onClick.AddListener(OnSlotClicked);
+using System.Collections; 
+using System.Collections.Generic; 
+using UnityEngine; 
+using UnityEngine.UI; 
+public class SlotUI : MonoBehaviour 
+{ 
+    public Image itemImage; public Text itemCountText; 
     }
-
-    public void OnSlotClicked()
-    {
-        if (item == null) return; // boş slot
-
-        // Bu item satılabilir mi kontrol
-        SellableItem sellable = item.GetComponent<SellableItem>();
-
-        if (sellable != null)
-        {
-            // Bu item balık yani satılabilir
-            FishSellPanel.Instance.SetItem(item, sellable.sellPrice);
-        }
-        else
-        {
-            // Balık değilse panel açılmayacak
-            Debug.Log("Bu item balık değil, satılamaz.");
-        }
-    }
-}
